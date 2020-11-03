3d0433fdf8ad2d31ba4c0ffbdafa93ec5d92376a
Jakub Charvat <jakcharvat@gmail.com>
2020-11-03 19:13:02 +0100

    Initial Commit

---
+++ Shared/DonutSimulationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DonutSimulationApp.swift
+//  Shared
+//
+//  Created by Jakub Charvat on 03.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct DonutSimulationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
